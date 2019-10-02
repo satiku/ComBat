57383c9957151f66bad10c2ccbd01b14a4c51f95
Hudson <zachary.hudson@igt.com>
2019-10-01 20:01:29 -0400

    refine module format and remove space ,etc

--- combat.py
+++ combat.py
@@ -5,6 +5,8 @@
  required modules   : os, argparse, datetime, jinja2, xlrd, netmiko
 """
 
+# pylint: disable=C0301
+
 import os
 import argparse
 import datetime
@@ -31,7 +33,7 @@ def pull_global_vars(global_vars_sheet):
         global_vars[rowvals[0]] = str(rowvals[1])
     return global_vars
 
-def pull_sheet_vars(sheet):
+def pull_sheet_vars(arg_sheet):
     """Pull vars from workbook sheet. Iterative values """
 
     return_sheet_vars = []
@@ -39,17 +41,17 @@ def pull_sheet_vars(sheet):
     var_type_list = []
 
 
-    for row in range(sheet.nrows):
+    for row in range(arg_sheet.nrows):
         if row == 0:
-            for index in sheet.row_values(row):
+            for index in arg_sheet.row_values(row):
                 var_index_list.append(index)
 
         elif row == 1:
-            for vartype in sheet.row_values(row):
+            for vartype in arg_sheet.row_values(row):
                 var_type_list.append(vartype)
 
         else:
-            rowvals = sheet.row_values(row)
+            rowvals = arg_sheet.row_values(row)
             new_dictionary = {}
             for i, var in enumerate(rowvals):
 
@@ -68,6 +70,7 @@ def pull_sheet_vars(sheet):
                     new_dictionary[var_index_list[i]] = var
 
                 elif var_type_list[i] == 'SPACE_DELIMITED':
+                    
                     if type(var) == float:
                         var_list = [int(var)]
                         new_dictionary[var_index_list[i]] = var_list
@@ -78,30 +81,32 @@ def pull_sheet_vars(sheet):
             return_sheet_vars.append(new_dictionary)
 
     return return_sheet_vars
-    
-def load_template(device):
-    # jinja template object
+
+def load_template(arg_device):
+    """Load Jinja2 device templates"""
+
     template_dir = r'C:\Users\ZCONINGFORD\Documents\Projects\ComBat\templates'
 
-    templateLoader = jinja2.FileSystemLoader( searchpath=template_dir)
-    templateEnv = jinja2.Environment( loader=templateLoader )
+    template_loader = jinja2.FileSystemLoader(searchpath=template_dir)
+    template_env = jinja2.Environment(loader=template_loader)
     # load template
     # print(device),
-    TEMPLATE = device
-    template = templateEnv.get_template( TEMPLATE )
 
-    return template
+    device_template = template_env.get_template(arg_device)
+
+    return device_template
 
 def write_config(snip, config_file):
-    # make dir path if it doesnt already exist
+    """Write to text file"""
+
     os.makedirs(os.path.dirname(config_file), exist_ok=True)
 
-    f = open(config_file, "w")
-    for line in snip.split('\n'):
+    file = open(config_file, "w")
+    for output_line in snip.split('\n'):
         # if line is not empty write it to file
-        if line.strip() != "":
-            f.write(line + '\n')
-    f.close()
+        if output_line.strip() != "":
+            file.write(output_line + '\n')
+    file.close()
 
 
 
@@ -110,36 +115,36 @@ if __name__ == '__main__':
 
 #    PROJECT_DIRS = os.path.expanduser('~') + "/Documents/Projects/"
 
-    parser = argparse.ArgumentParser(description='make some configs.')
+    parser = argparse.ArgumentParser(description='make some configs.')  # pylint: disable=C0103
+
     parser.add_argument('path', nargs=1, help='dir of the main.xlsx file')
-    parser.add_argument('--make', action='store_true', default=False,  help='make conifg')
 
-    parser.add_argument('--pull', action='store_true', default=False,  help='pull running conifg')
-
-
-
-    parser.add_argument('--push', action='store_true', default=False,  help='push conifg')
-    parser.add_argument('--gather', action='store_true', default=False,  help='gather')
+    parser.add_argument('--make', action='store_true', default=False, help='make conifg')
+    parser.add_argument('--pull', action='store_true', default=False, help='pull running conifg')
+    parser.add_argument('--push', action='store_true', default=False, help='push conifg')
+    parser.add_argument('--gather', action='store_true', default=False, help='gather')
 
 
-    args = parser.parse_args()
-#    PROJECT = str(args.path[0])
+    args = parser.parse_args()  # pylint: disable=C0103
     PROJECT_DIR = str(args.path[0])
-    project_workbook = {}
 
-    PROJECT_WORKBOOK = pull_workbook(PROJECT_DIR, "main.xlsm" )
+    project_workbook = {}   # pylint: disable=C0103
+
+    PROJECT_WORKBOOK = pull_workbook(PROJECT_DIR, "main.xlsm")
+
     for sheet in PROJECT_WORKBOOK.sheet_names():
         project_workbook[sheet] = pull_sheet_vars(PROJECT_WORKBOOK.sheet_by_name(sheet))
+
     project_workbook['data_global'] = pull_global_vars(PROJECT_WORKBOOK.sheet_by_name('data_global'))
 
 
-    if args.make == True:
+    if args.make:
         print('{:15}{:25}{:25}'.format("device name", "template fille", "input file"))
         print("+--------------------------------------------------------------+")
         for device in project_workbook['MAKE']:
 
             #print(PROJECT_DIR, "/INPUT/" + device['data_file'])
-            device_workbook = pull_workbook(PROJECT_DIR, "INPUT/" + device['data_file'] )
+            device_workbook = pull_workbook(PROJECT_DIR, "INPUT/" + device['data_file'])
             sheet_vars = {}
 
             # extract data from workbook
@@ -152,17 +157,17 @@ if __name__ == '__main__':
 
             sheet_vars['data_global']['site_prefix'] = project_workbook['data_global']['site_prefix']
 
-            device['workbook_data']= sheet_vars
+            device['workbook_data'] = sheet_vars
 
-            print('{:15}{:25}{:25}'.format(device['device'],device['template_file'], device['data_file']))
+            print('{:15}{:25}{:25}'.format(device['device'], device['template_file'], device['data_file']))
 
-            template = load_template( device['template_file'] )
+            template = load_template(device['template_file'])
             Snip = template.render(**sheet_vars)
 
-            CONFIG_FILE  = PROJECT_DIR + "/MAKE/" + device['device'] + ".txt"
-            write_config(Snip , CONFIG_FILE)
+            CONFIG_FILE = PROJECT_DIR + "/MAKE/" + device['device'] + ".txt"
+            write_config(Snip, CONFIG_FILE)
 
-    if args.pull == True:
+    if args.pull:
         print('{:35}{:15}'.format("device name", "IP"))
         print("+--------------------------------------------------------------+")
 
@@ -183,7 +188,7 @@ if __name__ == '__main__':
 
             if device['device_type'] == "fortinet":
                 output = net_connect.send_command_timing('config vdom', delay_factor=4)
-                output = net_connect.send_command_timing('edit ' + device['vslice'] , delay_factor=4)
+                output = net_connect.send_command_timing('edit ' + device['vslice'], delay_factor=4)
                 output = net_connect.send_command('show ')
 
 
@@ -191,8 +196,8 @@ if __name__ == '__main__':
                 output = net_connect.send_command("show run")
 
 
-            Pull_FILE  = PROJECT_DIR + "/PULL/" + datetime.datetime.now().strftime("%Y-%m-%d %H.%M.%S ") + device['device'] + ".txt"
-            write_config(output , Pull_FILE)
+            Pull_FILE = PROJECT_DIR + "/PULL/" + datetime.datetime.now().strftime("%Y-%m-%d %H.%M.%S ") + device['device'] + ".txt"
+            write_config(output, Pull_FILE)
 
 
 
@@ -200,7 +205,7 @@ if __name__ == '__main__':
 
 
 
-    if args.push == True:
+    if args.push:
         print('{:35}{:15}'.format("device name", "IP"))
         print("+--------------------------------------------------------------+")
 
@@ -216,7 +221,7 @@ if __name__ == '__main__':
                 'password': device['password'],
             }
 
-            CONFIG_FILE  = PROJECT_DIR + "/MAKE/" + device['device'] + ".txt"
+            CONFIG_FILE = PROJECT_DIR + "/MAKE/" + device['device'] + ".txt"
 
             # confirm
             wait = input("YOU ARE ABOUT TO PUSH CHANGES !!! PRESS ENTER TO CONTINUE.")
@@ -227,7 +232,7 @@ if __name__ == '__main__':
 
             if device['device_type'] == "fortinet":
                 output = net_connect.send_command_timing('config vdom', delay_factor=4)
-                output = net_connect.send_command_timing('edit ' + device['vslice'] , delay_factor=4)
+                output = net_connect.send_command_timing('edit ' + device['vslice'], delay_factor=4)
 
 
                 print(net_connect.find_prompt())
@@ -236,13 +241,13 @@ if __name__ == '__main__':
 
 
 
-                PUSH_LOG_FILE  = PROJECT_DIR + "/PUSH/" + datetime.datetime.now().strftime("%Y-%m-%d %H.%M.%S ") + device['device'] + ".txt"
-                write_config(output , PUSH_LOG_FILE )
+                PUSH_LOG_FILE = PROJECT_DIR + "/PUSH/" + datetime.datetime.now().strftime("%Y-%m-%d %H.%M.%S ") + device['device'] + ".txt"
+                write_config(output, PUSH_LOG_FILE)
 
 
 
 
-    if args.gather == True:
+    if args.gather:
         print('{:35}{:15}'.format("device name", "IP"))
         print("+--------------------------------------------------------------+")
 
@@ -261,10 +266,10 @@ if __name__ == '__main__':
             net_connect = netmiko.ConnectHandler(**devices)
 
 
-        
+
             if device['device_type'] == "fortinet":
                 net_connect.send_command_timing('config vdom', delay_factor=4)
-                net_connect.send_command_timing('edit ' + device['vslice'] , delay_factor=4)
+                net_connect.send_command_timing('edit ' + device['vslice'], delay_factor=4)
 
                 output = net_connect.send_command('fnsysctl more  /etc/upd.dat')
                 final = output.split("|")[0] + '\n'
@@ -285,9 +290,9 @@ if __name__ == '__main__':
 
                 for line in output:
 
-                    if "Master:" in line or "Slave :" in line :
+                    if "Master:" in line or "Slave :" in line:
                         final += line + '\n'
 
 
-            Gather_FILE  = PROJECT_DIR + "/GATHER/" +  datetime.datetime.now().strftime("%Y-%m-%d %H.%M.%S ") + device['device'] + ".txt"
-            write_config(final , Gather_FILE)
+            Gather_FILE = PROJECT_DIR + "/GATHER/" +  datetime.datetime.now().strftime("%Y-%m-%d %H.%M.%S ") + device['device'] + ".txt"
+            write_config(final, Gather_FILE)
